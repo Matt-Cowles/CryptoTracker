643d80cbcfddc5fe398f7493ece863febf4faccb
Matt Cowles <selwocm@gmail.com>
2022-12-17 14:22:10 -0800

    connect mongoose and set upejs and basic express middleware

--- app.js
+++ app.js
@@ -2,7 +2,34 @@ import fetch from "node-fetch";
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
+import ejsMate from "ejs-mate";
+import path from "path";
+import url from "url";
 
-const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
-const listings = await response.json();
-console.log(data[0]);
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/crypto-tracker");
+}
+
+// New __dirname due to ES Modules
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/tracker", async (req, res) => {
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
+  const listings = await response.json();
+
+  console.log(listings);
+
+  res.send(listings);
+});
+
+app.listen(3000, (req, res) => console.log("listening on port 3000"));
