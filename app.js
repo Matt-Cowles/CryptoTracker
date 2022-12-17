a0ef280b9e963f5f090a28650355178dbb6a6d1f
Matt Cowles <selwocm@gmail.com>
2022-12-17 13:25:41 -0800

    install and import fetch from node-fetch. fetch crypto listing data from coingecko

---
+++ app.js
@@ -0,0 +1,5 @@
+import fetch from "node-fetch";
+
+const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
+const listings = await response.json();
+console.log(data[0]);
