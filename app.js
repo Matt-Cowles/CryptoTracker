410d3435fe7aeb4e9ef6b3d8faf58d45e67d1933
Matt Cowles <selwocm@gmail.com>
2022-12-17 13:29:50 -0800

    install express mongoose and ejs. import express and mongoose

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import fetch from "node-fetch";
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
 
 const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
 const listings = await response.json();
