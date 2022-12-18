2ca0db3c2f16bf262be9a7dba0ee91d34356655f
Matt Cowles <selwocm@gmail.com>
2022-12-17 16:58:45 -0800

    create mongoose model export

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose";
+import { Schema } from mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("User", UserSchema);
