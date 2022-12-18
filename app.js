8f061f577136a46995218bc438d9a871f2c666b8
Matt Cowles <selwocm@gmail.com>
2022-12-17 16:29:43 -0800

    render the index crypto listing page

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.get("/tracker", async (req, res) => {
   const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
   const listings = await response.json();
 
-  console.log(listings);
+  // console.log(listings);
 
-  res.send(listings);
+  res.render("./index", { listings });
 });
 
 app.listen(3000, (req, res) => console.log("listening on port 3000"));
